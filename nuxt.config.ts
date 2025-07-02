785024dc538ca3ed37ff15b9abab66864e64a0b3
Dehou23333 <104069850+Dehou23333-awa@users.noreply.github.com>
2025-07-02 16:12:39 +0800

    feat: Add build info component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,6 @@
+import { getGitInfo } from './utils/git-info.js'
+const gitInfo = getGitInfo()
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
@@ -8,6 +11,14 @@ export default defineNuxtConfig({
       wrangler: {
         name: 'dehou23333-awa-rks'
       }
+    }
+  },
+  runtimeConfig: {
+    public: {
+      gitCommit: gitInfo.hash,
+      gitDate: gitInfo.date,
+      gitBranch: gitInfo.branch,
+      buildTime: new Date().toISOString()
     }
   }
 })
